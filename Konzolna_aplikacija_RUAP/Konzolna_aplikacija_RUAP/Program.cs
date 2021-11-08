9d21cc09125590b6b1ac71430a1fbd7b0ac684d4
Markan <rmarkovic70@gmail.com>
2021-11-08 16:35:18 +0100

    Added VisualStudio HELLO WORLD project

---
+++ Konzolna_aplikacija_RUAP/Konzolna_aplikacija_RUAP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Konzolna_aplikacija_RUAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
